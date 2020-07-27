097fcefd336c022a2fa42bbb5514360c90bb6b60
Assis, Raphael A <raphael.a.assis@accenture.com>
2020-07-27 12:20:13 -0300

    adiciando o EF Core, e adicionando os contracts e inserindo o appsettings com a connectionStrings

---
+++ ProjetoClinicaMedicaDDD.Domain/Contracts/Repositories/IAtendimentoRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoClinicaMedica.Domain.Contracts.Repositories
+{
+    public interface IAtendimentoRepository
+    {
+    }
+}
---
+++ ProjetoClinicaMedicaDDD.Domain/Contracts/Repositories/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoClinicaMedica.Domain.Contracts.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(T obj);
+        List<T> GetAll();
+        T GetById(int id);
+    }
+}
