5ab7bd36bb62f0004c3cb7d6ec9d0e01588519a8
Assis, Raphael A <raphael.a.assis@accenture.com>
2020-08-14 13:28:55 -0300

    criando repositories e domain da aplicacao

--- ProjetoClinicaMedicaDDD.Domain/Contracts/Repositories/IAtendimentoRepository.cs
+++ ProjetoClinicaMedicaDDD.Domain/Contracts/Repositories/IAtendimentoRepository.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using ProjetoClinicaMedica.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ProjetoClinicaMedica.Domain.Contracts.Repositories
 {
-    public interface IAtendimentoRepository
+    public interface IAtendimentoRepository : IBaseRepository<Atendimento>
     {
     }
 }
